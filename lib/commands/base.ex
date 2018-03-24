af5e5833c0edebf4919a99276433c80d375b3028
kornakh <larkin@laposte.net>
2018-03-24 16:57:54 +0100

    add .roll

--- lib/commands/base.ex
+++ lib/commands/base.ex
@@ -35,6 +35,32 @@ defmodule KatoMarika.Commands.Base do
   end
 
   @doc """
+  <*x*d*y*> : je lance *x* dés à *y* faces
+  """
+  Cogs.def roll(dices\\"1d20") do
+    [nbDice, face]=String.split(String.downcase(dices), "d")
+      |> Enum.map(fn(x)->String.to_integer(x)end)
+
+
+    tasks=for _ <- 1..nbDice do
+     Task.async(fn->:rand.uniform(face)end)
+    end
+    tasks_with_results = Task.yield_many(tasks, 50000000)
+    tasks_with_results = Enum.map(tasks_with_results, fn {_,res} ->
+      elem(res,1)
+    end)
+    total = Enum.reduce(tasks_with_results, fn(x, acc)-> x+acc end)
+
+    results = Enum.join(tasks_with_results, ", ")
+    s1 = if nbDice>1 do "s" else "" end
+    s2 = if face>1 do "s" else "" end
+    total = if nbDice>1 do "Ce qui me fait un total de **#{total}**" else "" end
+    Cogs.say "J'ai lancé **#{nbDice}** dé#{s1} à **#{face}** face#{s2}\n"
+      <> "J'obtiens le#{s1} résultat#{s1} suivant#{s1} : **#{results}**\n"
+      <> total
+  end
+
+  @doc """
   : Affiche l'aide
   """
   Cogs.def help do
