9ca4703c5b72144589a4d78ef5b2c73cffa79a24
kornakh <larkin@laposte.net>
2018-03-24 15:24:12 +0100

    warn user in case of no config file

--- lib/KatoMarikaDiscord.ex
+++ lib/KatoMarikaDiscord.ex
@@ -5,11 +5,13 @@ defmodule KatoMarikaDiscord do
 
 # id : 426055346079924234
   def start(_type, _args) do
-
-    run = Client.start(Application.get_env(:katomarikadiscord, :key))
-    Alchemy.Cogs.set_prefix(".")
-    use KatoMarika.Commands.Base
-    use KatoMarika.Events.EventHandler
-    run
+    case Application.get_env(:katomarikadiscord, :key, :error) do
+      :error -> IO.puts :stderr, IO.ANSI.red<>"no config file, copy configExemple.exs to config.exs and modify key attribute to your key"
+      key ->    run = Client.start(Application.get_env(:katomarikadiscord, :key))
+                Alchemy.Cogs.set_prefix(".")
+                use KatoMarika.Commands.Base
+                use KatoMarika.Events.EventHandler
+                run
+    end
   end
 end
